aceef2cc4383a8423935f55846a561465319b556
José Ignacio Alamos <jialamos@uc.cl>
2017-06-02 18:42:33 -0400

    implement send command

--- tests/openthread_sock_udp/main.c
+++ tests/openthread_sock_udp/main.c
@@ -23,6 +23,8 @@
 #include "shell.h"
 #include "shell_commands.h"
 
+static ot_udp_context_t ctx;
+
 static void _recv(void *aContext, otMessage *aMessage, const otMessageInfo *aMessageInfo)
 {
 	puts("It works!");
@@ -43,9 +45,7 @@ int ip_addr(int argc, char **argv)
 	return 0;
 }
 
-#if 0
-static void send(char *addr_str, char *port_str, char *data, unsigned int num,
-                 unsigned int delay)
+static void _send(char *addr_str, char *port_str, char *data, size_t len)
 {
     uint16_t port;
     ipv6_addr_t addr;
@@ -62,86 +62,20 @@ static void send(char *addr_str, char *port_str, char *data, unsigned int num,
         return;
     }
 
-    for (unsigned int i = 0; i < num; i++) {
-        gnrc_pktsnip_t *payload, *udp, *ip;
-        unsigned payload_size;
-        /* allocate payload */
-        payload = gnrc_pktbuf_add(NULL, data, strlen(data), GNRC_NETTYPE_UNDEF);
-        if (payload == NULL) {
-            puts("Error: unable to copy data to packet buffer");
-            return;
-        }
-        /* store size for output */
-        payload_size = (unsigned)payload->size;
-        /* allocate UDP header, set source port := destination port */
-        udp = gnrc_udp_hdr_build(payload, port, port);
-        if (udp == NULL) {
-            puts("Error: unable to allocate UDP header");
-            gnrc_pktbuf_release(payload);
-            return;
-        }
-        /* allocate IPv6 header */
-        ip = gnrc_ipv6_hdr_build(udp, NULL, &addr);
-        if (ip == NULL) {
-            puts("Error: unable to allocate IPv6 header");
-            gnrc_pktbuf_release(udp);
-            return;
-        }
-        /* send packet */
-        if (!gnrc_netapi_dispatch_send(GNRC_NETTYPE_UDP, GNRC_NETREG_DEMUX_CTX_ALL, ip)) {
-            puts("Error: unable to locate UDP thread");
-            gnrc_pktbuf_release(ip);
-            return;
-        }
-        /* access to `payload` was implicitly given up with the send operation above
-         * => use temporary variable for output */
-        printf("Success: sent %u byte(s) to [%s]:%u\n", payload_size, addr_str,
-               port);
-        xtimer_usleep(delay);
-    }
+	ctx.ip_addr = addr;
+	ctx.port = port;
+	ctx.tx_buf = data;
+	ctx.tx_len = len;
+
+	ctx.type = OPENTHREAD_NET_SEND;
+
+	ot_call_command("udp", &ctx, NULL);
 }
 
 int udp_cmd(int argc, char **argv)
 {
-    if (argc < 2) {
-        printf("usage: %s [send|server]\n", argv[0]);
-        return 1;
-    }
-
     if (strcmp(argv[1], "send") == 0) {
-        uint32_t num = 1;
-        uint32_t delay = 1000000;
-        if (argc < 5) {
-            printf("usage: %s send <addr> <port> <data> [<num> [<delay in us>]]\n",
-                   argv[0]);
-            return 1;
-        }
-        if (argc > 5) {
-            num = atoi(argv[5]);
-        }
-        if (argc > 6) {
-            delay = atoi(argv[6]);
-        }
-        send(argv[2], argv[3], argv[4], num, delay);
-    }
-    else if (strcmp(argv[1], "server") == 0) {
-        if (argc < 3) {
-            printf("usage: %s server [start|stop]\n", argv[0]);
-            return 1;
-        }
-        if (strcmp(argv[2], "start") == 0) {
-            if (argc < 4) {
-                printf("usage %s server start <port>\n", argv[0]);
-                return 1;
-            }
-            start_server(argv[3]);
-        }
-        else if (strcmp(argv[2], "stop") == 0) {
-            stop_server();
-        }
-        else {
-            puts("error: invalid command");
-        }
+        _send(argv[2], argv[3], argv[4], strlen(argv[4]));
     }
     else {
         puts("error: invalid command");
@@ -149,16 +83,14 @@ int udp_cmd(int argc, char **argv)
     return 0;
 }
 
-#endif
 static const shell_command_t shell_commands[] = {
-//    { "udp", "send data over UDP", udp_cmd },
+    { "udp", "send data over UDP", udp_cmd },
     { "ipaddr", "print ip addresses", ip_addr },
     { NULL, NULL, NULL }
 };
 
 int main(void)
 {
-	ot_udp_context_t ctx;
 
 	ctx.type = OPENTHREAD_NET_SOCKET_CREATE;
 	ctx.cb = _recv;
