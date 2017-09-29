bf41a8f1a4e01090ebb25a6d6588dabc59fa5656
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-09-29 17:35:33 +0200

    pkg/semtech-loramac: adapt to last version of sx127x

--- pkg/semtech-loramac/contrib/sys_radio.c
+++ pkg/semtech-loramac/contrib/sys_radio.c
@@ -1,5 +1,6 @@
 #include "loramac/board_definitions.h"
 #include "sx127x.h"
+#include "sx127x_internal.h"
 
 /*!
  * \ Sx1276 driver for use of LoRaMac
@@ -41,12 +42,12 @@ void SX1276Init(RadioEvents_t *events)
 
 RadioState_t SX1276GetStatus(void)
 {
-    return (RadioState_t)sx127x_get_status(dev_ptr);
+    return (RadioState_t)sx127x_get_state(dev_ptr);
 }
 
 void SX1276SetModem(RadioModems_t modem)
 {
-    sx127x_set_modem(dev_ptr, (sx1276_radio_modems_t) modem);
+    sx127x_set_modem(dev_ptr, (uint8_t)modem);
 }
 
 void SX1276SetChannel(uint32_t freq)
@@ -60,71 +61,69 @@ bool SX1276IsChannelFree(RadioModems_t modem, uint32_t freq,
     return sx127x_is_channel_free(dev_ptr, freq, rssiThresh);
 }
 
-uint32_t SX1276Random( void )
+uint32_t SX1276Random(void)
 {
      return sx127x_random(dev_ptr);
 }
 
-void SX1276SetRxConfig( RadioModems_t modem, uint32_t bandwidth,
-                         uint32_t datarate, uint8_t coderate,
-                         uint32_t bandwidthAfc, uint16_t preambleLen,
-                         uint16_t symbTimeout, bool fixLen,
-                         uint8_t payloadLen,
-                         bool crcOn, bool freqHopOn, uint8_t hopPeriod,
-                         bool iqInverted, bool rxContinuous )
+void SX1276SetRxConfig(RadioModems_t modem, uint32_t bandwidth,
+                       uint32_t spreading_factor, uint8_t coding_rate,
+                       uint32_t bandwidthAfc, uint16_t preambleLen,
+                       uint16_t symbTimeout, bool fixLen,
+                       uint8_t payloadLen,
+                       bool crcOn, bool freqHopOn, uint8_t hopPeriod,
+                       bool iqInverted, bool rxContinuous)
 {
-    sx127x_lora_settings_t settings;
-    dev_ptr->settings.modem = modem;
     (void) bandwidthAfc;
-    settings.bandwidth = bandwidth + 7;
-    settings.coderate = coderate;
-    settings.datarate = datarate;
-    settings.crc_on = crcOn;
-    settings.freq_hop_on = freqHopOn;
-    settings.hop_period = hopPeriod;
-    settings.implicit_header = false;
-    settings.iq_inverted = iqInverted;
-    settings.low_datarate_optimize = fixLen;
-    settings.payload_len = payloadLen;
-    settings.power = 14;
-    settings.preamble_len = preambleLen;
-    settings.rx_continuous = rxContinuous;
-    settings.tx_timeout = 3 * 1000 * 1000; // base units us
-    settings.rx_timeout = symbTimeout;
-    sx1276_configure_lora(dev_ptr, &settings);
+    sx127x_set_modem(dev_ptr, modem);
+    sx127x_set_freq_hop(dev_ptr, freqHopOn);
+    sx127x_set_bandwidth(dev_ptr, bandwidth);
+    sx127x_set_coding_rate(dev_ptr, coding_rate);
+    sx127x_set_spreading_factor(dev_ptr, spreading_factor);
+    sx127x_set_crc(dev_ptr, crcOn);
+    sx127x_set_freq_hop(dev_ptr, freqHopOn);
+    sx127x_set_hop_period(dev_ptr, hopPeriod);
+    sx127x_set_fixed_header_len_mode(dev_ptr, false);
+    sx127x_set_iq_invert(dev_ptr, iqInverted);
+    // settings.low_datarate_optimize = fixLen;
+    // settings.payload_len = payloadLen;sx127x_set_tx_power(dev_ptr, power);
+    sx127x_set_tx_power(dev_ptr, SX127X_RADIO_TX_POWER);
+    sx127x_set_preamble_length(dev_ptr, preambleLen);
+    sx127x_set_rx_single(dev_ptr, !rxContinuous);
+    sx127x_set_rx(dev_ptr);
+    sx127x_set_tx_timeout(dev_ptr, 3 * 1000 * 1000); /* base units us */
+    sx127x_set_rx_timeout(dev_ptr, symbTimeout);
 }
 
 void SX1276SetTxConfig(RadioModems_t modem, int8_t power, uint32_t fdev,
-                       uint32_t bandwidth, uint32_t datarate,
-                       uint8_t coderate, uint16_t preambleLen,
+                       uint32_t bandwidth, uint32_t spreading_factor,
+                       uint8_t coding_rate, uint16_t preambleLen,
                        bool fixLen, bool crcOn, bool freqHopOn,
                        uint8_t hopPeriod, bool iqInverted, uint32_t timeout)
 {
-    sx127x_lora_settings_t settings;
     (void) fdev;
-    dev_ptr->settings.modem = modem;
-    settings.bandwidth = bandwidth + 7;
-    settings.coderate = coderate;
-    settings.datarate = datarate;
-    settings.crc_on = crcOn;
-    settings.freq_hop_on = freqHopOn;
-    settings.hop_period = hopPeriod;
-    settings.implicit_header = false;
-    settings.iq_inverted = iqInverted;
-    settings.low_datarate_optimize = fixLen;
-    settings.payload_len = 0;
-    settings.power = power;
-    settings.preamble_len = preambleLen;
-    settings.rx_continuous = true;
-    settings.tx_timeout = timeout * 1000; // base unit us, LoRaMAC ms
-    settings.rx_timeout = 10;
-    sx127x_configure_lora(dev_ptr, &settings);
+    sx127x_set_modem(dev_ptr, modem);
+    sx127x_set_freq_hop(dev_ptr, freqHopOn);
+    sx127x_set_bandwidth(dev_ptr, bandwidth);
+    sx127x_set_coding_rate(dev_ptr, coding_rate);
+    sx127x_set_spreading_factor(dev_ptr, spreading_factor);
+    sx127x_set_crc(dev_ptr, crcOn);
+    sx127x_set_freq_hop(dev_ptr, freqHopOn);
+    sx127x_set_hop_period(dev_ptr, hopPeriod);
+    sx127x_set_fixed_header_len_mode(dev_ptr, false);
+    sx127x_set_iq_invert(dev_ptr, iqInverted);
+    // settings.low_datarate_optimize = fixLen;
+    // settings.payload_len = 0;
+    sx127x_set_tx_power(dev_ptr, power);
+    sx127x_set_preamble_length(dev_ptr, preambleLen);
+    sx127x_set_rx_single(dev_ptr, false); 
+    sx127x_set_tx_timeout(dev_ptr, timeout * 1000); /* base unit us, LoRaMAC ms */
+    sx127x_set_rx_timeout(dev_ptr, 10);
 }
 
 uint32_t SX1276GetTimeOnAir(RadioModems_t modem, uint8_t pktLen)
 {
-    return sx1276_get_time_on_air(dev_ptr, (sx1276_radio_modems_t) modem,
-                                  pktLen);
+    return sx127x_get_time_on_air(dev_ptr, pktLen);
 }
 
 void SX1276Send(uint8_t *buffer, uint8_t size)
@@ -144,7 +143,8 @@ void SX1276SetStby(void)
 
 void SX1276SetRx(uint32_t timeout)
 {
-    sx127x_set_rx(dev_ptr, timeout * 1000); // base unit us, LoRaMAC ms
+    sx127x_set_rx_timeout(dev_ptr, timeout * 1000);
+    sx127x_set_rx(dev_ptr);
 }
 
 
@@ -155,7 +155,7 @@ void SX1276StartCad(void)
 
 int16_t SX1276ReadRssi(RadioModems_t modem)
 {
-    dev_ptr->settings.modem = modem;
+    sx127x_set_modem(dev_ptr, (uint8_t)modem);
     return sx127x_read_rssi(dev_ptr);
 }
 
@@ -181,7 +181,7 @@ void SX1276ReadBuffer(uint8_t addr, uint8_t *buffer, uint8_t size)
 
 void SX1276SetMaxPayloadLength(RadioModems_t modem, uint8_t max)
 {
-    sx127x_set_max_payload_len(dev_ptr, (sx1276_radio_modems_t) modem, max);
+    sx127x_set_max_payload_len(dev_ptr, max);
 }
 
 bool SX1276CheckRfFrequency(uint32_t frequency)
