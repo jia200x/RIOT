1a53e3b97a6165ece6c8cf239ba02d9cced6b213
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-11-30 16:30:30 +0100

    implement proper channel picking

--- sys/net/gnrc/link_layer/gnrc_lorawan/gnrc_lorawan_region.c
+++ sys/net/gnrc/link_layer/gnrc_lorawan/gnrc_lorawan_region.c
@@ -36,15 +36,40 @@ uint8_t gnrc_lorawan_rx1_get_dr_offset(uint8_t dr_up, uint8_t dr_offset) {
     return rx1_dr_offset[dr_up][dr_offset];
 }
 
+/*TODO: Reimplement */
+static size_t _get_num_used_channels(void) {
+    size_t count = 0;
+    for(int i=0;i<16;i++) {
+        if(lorawan_channels[i])
+            count++;
+    }
+    return count;
+}
+
+/*TODO: Reimplement */
+static uint32_t _get_nth_channel(size_t n) {
+    int i=0;
+    uint32_t channel = 0;
+    while(n) {
+        if(lorawan_channels[i]) {
+            n--;
+            channel = lorawan_channels[i];
+            i++;
+        }
+    }
+    return channel;
+}
+
 uint32_t gnrc_lorawan_pick_channel(gnrc_netif_t *netif) {
     /* TODO: Implement when add/remove channel gets implemented */
     netdev_t *netdev = netif->dev;
     uint32_t random_number;
     netdev->driver->get(netdev, NETOPT_RANDOM, &random_number, sizeof(random_number));
     
-    return lorawan_channels[random_number % 3];
+    return _get_nth_channel(random_number % _get_num_used_channels());
 }
 
+/*TODO: Reimplement! */
 void gnrc_lorawan_process_cflist(gnrc_netif_t *netif, uint8_t *cflist) {
     (void) netif;
     /* TODO: Check CFListType to 0 */
