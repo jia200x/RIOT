673c275fd0a40bf7f99d07487e411f160f793431
Jose Alamos <jose.alamos@haw-hamburg.de>
2019-04-09 16:45:59 +0200

    CHERRYPICK_ME:pkg/openwsn: add code for mac only usage

--- pkg/openwsn/contrib/openwsn.c
+++ pkg/openwsn/contrib/openwsn.c
@@ -18,6 +18,11 @@
 #include "board_ow.h"
 #include "radio.h"
 
+#ifdef OW_MAC_ONLY
+#include "errno.h"
+#include "event.h"
+#endif
+
 #define ENABLE_DEBUG    (0)
 #include "debug.h"
 
@@ -27,6 +32,12 @@
 
 static kernel_pid_t _pid = KERNEL_PID_UNDEF;
 static char _stack[OW_SCHED_STACKSIZE];
+
+#ifdef OW_MAC_ONLY
+extern void mlme_sync_indication(void);
+extern void mlme_sync_loss_indication(void);
+extern void ow_mcps_data_confirm(int status);
+#endif
 
 static void *_event_loop(void *arg);
 
@@ -50,9 +61,74 @@ void openwsn_bootstrap(void)
     }
 }
 
+#ifdef OW_MAC_ONLY
+static void _sixtop_management_fired(event_t *event)
+{
+    timer_sixtop_management_fired();
+}
+
+static void _sixtop_sendEb_fired(event_t *event)
+{
+    timer_sixtop_sendEb_fired();
+}
+
+static void _sixtop_notify_send_done(event_t *event)
+{
+    task_sixtopNotifSendDone();
+}
+
+static void _sixtop_notify_receive(event_t *event)
+{
+    task_sixtopNotifReceive();
+}
+
+static void _indicate_sync(event_t *event)
+{
+    mlme_sync_indication();
+}
+
+static void _indicate_sync_loss(event_t *event)
+{
+    mlme_sync_loss_indication();
+}
+
+event_t ev_ieee154e_indicate_sync = { .handler = _indicate_sync };
+event_t ev_ieee154e_indicate_sync_loss = { .handler = _indicate_sync_loss };
+
+event_t ev_sixtop_management_fired = { .handler = _sixtop_management_fired };
+event_t ev_sixtop_sendEb_fired = { .handler = _sixtop_sendEb_fired };
+event_t ev_sixtop_notify_send_done = { .handler = _sixtop_notify_send_done };
+event_t ev_sixtop_notify_receive = { .handler = _sixtop_notify_receive };
+
+event_queue_t queue;
+
+void ieee154e_indicate_sync(void)
+{
+    event_post(&queue, &ev_ieee154e_indicate_sync);
+}
+
+void ieee154e_indicate_sync_loss(void)
+{
+    event_post(&queue, &ev_ieee154e_indicate_sync_loss);
+}
+#endif
+
 static void *_event_loop(void *arg)
 {
     DEBUG("[openwsn_bootstrap]: init openstack\n");
+#ifdef OW_MAC_ONLY
+    event_queue_init(&queue);
+    openserial_init();
+    idmanager_init();    // call first since initializes EUI64 and isDAGroot
+    openqueue_init();
+    openrandom_init();
+    opentimers_init();
+    ieee154e_init();
+    schedule_init();
+    sixtop_init();
+    neighbors_init();
+    event_loop(&queue);
+#else
     openstack_init();
 
     DEBUG("[openwsn_bootstrap]: init scheduler\n");
@@ -61,6 +137,7 @@ static void *_event_loop(void *arg)
     DEBUG("[openwsn_bootstrap]: start scheduler");
     /* starts the OpenWSN scheduler which contains a loop */
     scheduler_start();
+#endif
     return NULL;
 }
 
--- pkg/openwsn/include/openwsn.h
+++ pkg/openwsn/include/openwsn.h
@@ -23,6 +23,8 @@
 #ifndef OPENWSN_H
 #define OPENWSN_H
 
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -30,6 +32,59 @@ extern "C" {
 #define OW_SCHED_MSG_TYPE_EVENT    0x667    // TODO ADAPT!!!
 #define OW_NETDEV_MSG_TYPE_EVENT   0x666    // TODO ADAPT!!!
 
+typedef enum {
+    OW_CELL_ADV=0,
+    OW_CELL_RX,
+    OW_CELL_TX
+} ow_cell_t;
+
+typedef enum {
+    OW_ROLE_PAN_COORDINATOR=0,
+    OW_ROLE_COORDINATOR,
+    OW_ROLE_LEAF
+} ow_role_t;
+
+/**
+ * @brief Perform MLME-SET_LINK request.
+ *
+ * @param add True if it's requested to add a cell. False otherwise
+ * @param slot_offset Slot offset.
+ * @param type Cell type
+ * @param shared True if the link is shared with other nodes. False otherwise
+ * @param ch_offset Channel offset
+ * @param addr The address of the neighbour. Set to NULL in case of ADV cell.
+ *
+ * @return 0 on success
+ * @return -EINVAL on failure
+ */
+int openwsn_mlme_set_link_request(bool add, int slot_offset, ow_cell_t type, bool shared, int ch_offset,
+        uint8_t *addr);
+
+/**
+ * @brief Set the role
+ *
+ * @param role the new role
+ */
+void openwsn_mlme_set_role(ow_role_t role);
+
+/**
+ * @brief Perform MLME-SET_SLOTFRAME request
+ *
+ * @param size length of the new slotframe
+ *
+ * @return 0 on success.
+ * @return -EINVAL on failure
+ */
+int openwsn_mlme_set_slotframe_request(uint8_t size);
+
+/**
+ * @brief Perform MCPS-DATA request
+ *
+ * @param dest 64-bit destination address
+ * @param data data to be sent
+ * @param data_len length of the data
+ */
+void openwsn_mcps_data_request(char *dest, char *data, size_t data_len);
 
 /**
  * @brief   Initializes the OpenWSN stack.
