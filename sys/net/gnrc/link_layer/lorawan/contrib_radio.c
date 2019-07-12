1c686b38db96c6fe5a495807998462fd74346c64
Jose Alamos <jose.alamos@haw-hamburg.de>
2019-07-12 14:42:45 +0200

    partially remove netdev layered

--- sys/net/gnrc/link_layer/lorawan/contrib_radio.c
+++ sys/net/gnrc/link_layer/lorawan/contrib_radio.c
@@ -14,44 +14,50 @@
 
 void gnrc_lorawan_radio_sleep(gnrc_lorawan_t *mac)
 {
+    netdev_t *dev = mac->netdev.lower;
     netopt_state_t state = NETOPT_STATE_SLEEP;
-    netdev_set_pass((netdev_t *) mac, NETOPT_STATE, &state, sizeof(state));
+    dev->driver->set(dev, NETOPT_STATE, &state, sizeof(state));
 }
 
 void gnrc_lorawan_radio_set_cr(gnrc_lorawan_t *mac, uint8_t cr)
 {
-    netdev_set_pass(&mac->netdev, NETOPT_CODING_RATE, &cr, sizeof(cr));
+    netdev_t *dev = mac->netdev.lower;
+    dev->driver->set(dev, NETOPT_CODING_RATE, &cr, sizeof(cr));
 }
 
 void gnrc_lorawan_radio_set_syncword(gnrc_lorawan_t *mac, uint8_t syncword)
 {
-    netdev_set_pass(&mac->netdev, NETOPT_SYNCWORD, &syncword, sizeof(syncword));
+    netdev_t *dev = mac->netdev.lower;
+    dev->driver->set(dev, NETOPT_SYNCWORD, &syncword, sizeof(syncword));
 }
 
 void gnrc_lorawan_radio_set_frequency(gnrc_lorawan_t *mac, uint32_t channel)
 {
-    netdev_set_pass(&mac->netdev, NETOPT_CHANNEL_FREQUENCY, &channel, sizeof(channel));
+    netdev_t *dev = mac->netdev.lower;
+    dev->driver->set(dev, NETOPT_CHANNEL_FREQUENCY, &channel, sizeof(channel));
 }
 
 void gnrc_lorawan_radio_set_iq_invert(gnrc_lorawan_t *mac, int invert)
 {
+    netdev_t *dev = mac->netdev.lower;
     netopt_enable_t iq_invert = invert;
-    netdev_set_pass(&mac->netdev, NETOPT_IQ_INVERT, &iq_invert, sizeof(iq_invert));
+    dev->driver->set(dev, NETOPT_IQ_INVERT, &iq_invert, sizeof(iq_invert));
 }
 
 void gnrc_lorawan_radio_set_rx_symbol_timeout(gnrc_lorawan_t *mac, uint16_t timeout)
 {
-    /* Switch to single listen mode */
+    netdev_t *dev = mac->netdev.lower;
     const netopt_enable_t single = true;
-    netdev_set_pass(&mac->netdev, NETOPT_SINGLE_RECEIVE, &single, sizeof(single));
+    dev->driver->set(dev, NETOPT_SINGLE_RECEIVE, &single, sizeof(single));
 
-    netdev_set_pass(&mac->netdev, NETOPT_RX_SYMBOL_TIMEOUT, &timeout, sizeof(timeout));
+    dev->driver->set(dev, NETOPT_RX_SYMBOL_TIMEOUT, &timeout, sizeof(timeout));
 }
 
 void gnrc_lorawan_radio_rx_on(gnrc_lorawan_t *mac)
 {
+    netdev_t *dev = mac->netdev.lower;
     uint8_t state = NETOPT_STATE_RX;
-    netdev_set_pass(&mac->netdev, NETOPT_STATE, &state, sizeof(state));
+    dev->driver->set(dev, NETOPT_STATE, &state, sizeof(state));
 }
 
 void gnrc_lorawan_radio_set_sf(gnrc_lorawan_t *mac, uint8_t sf)
@@ -68,7 +74,8 @@ void gnrc_lorawan_radio_set_bw(gnrc_lorawan_t *mac, uint8_t bw)
 
 void gnrc_lorawan_radio_send(gnrc_lorawan_t *mac, iolist_t *io)
 {
-    if (netdev_send_pass(&mac->netdev, io) == -ENOTSUP) {
+    netdev_t *dev = mac->netdev.lower;
+    if (dev->driver->send(dev, io) == -ENOTSUP) {
         DEBUG("gnrc_lorawan: Cannot send: radio is still transmitting");
     }
 }
