c6e35c41e29972f7a09bb8856b6cb09645ef1cce
Jose Alamos <jose.alamos@haw-hamburg.de>
2020-07-24 16:13:06 +0200

    fix missing release mac

--- sys/net/gnrc/link_layer/lorawan/gnrc_lorawan_mcps.c
+++ sys/net/gnrc/link_layer/lorawan/gnrc_lorawan_mcps.c
@@ -357,7 +357,7 @@ void gnrc_lorawan_mcps_request(gnrc_lorawan_t *mac, const mcps_request_t *mcps_r
 
     if (!gnrc_lorawan_mac_acquire(mac) || mac->state != LORAWAN_STATE_IDLE) {
         mcps_confirm->status = -EBUSY;
-        return;
+        goto out;
     }
 
     if (mcps_request->data.port < LORAMAC_PORT_MIN ||
