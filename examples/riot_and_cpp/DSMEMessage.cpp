f4b341a709c1d78ccf1314dc1409741cfab20cbc
Jose Alamos <jose@alamos.cc>
2021-09-21 11:36:30 +0200

    it works

--- examples/riot_and_cpp/DSMEMessage.cpp
+++ examples/riot_and_cpp/DSMEMessage.cpp
@@ -6,6 +6,11 @@ namespace dsme {
 void DSMEMessage::prependFrom(DSMEMessageElement *msg)
 {
     gnrc_pktsnip_t *head = gnrc_pktbuf_add(pkt, NULL, msg->getSerializationLength(), GNRC_NETTYPE_UNDEF);
+    DSME_ASSERT(msg->getSerializationLength() <= 127);
+    DSME_ASSERT(head);
+    int res = gnrc_pktbuf_merge(head);
+    DSME_ASSERT(res == 0);
+    DSME_ASSERT(gnrc_pkt_len(head) <= 127);
     pkt = head;
     assert(head);
     Serializer s((uint8_t*) head->data, SERIALIZATION);
@@ -36,7 +41,9 @@ int DSMEMessage::loadBuffer(size_t len)
 {
     int res = -ENOBUFS;
     gnrc_pktsnip_t *pkt = gnrc_pktbuf_add(NULL, NULL, len, GNRC_NETTYPE_UNDEF);
+    DSME_ASSERT(len <= 127);
     if (pkt == NULL) {
+        DSME_ASSERT(false);
         goto end;
     }
     this->pkt = pkt; 
@@ -50,6 +57,7 @@ int DSMEMessage::dropHdr(size_t len)
 {
     gnrc_pktsnip_t *hdr = gnrc_pktbuf_mark(this->pkt, len, GNRC_NETTYPE_UNDEF);
     if (!hdr) {
+        DSME_ASSERT(false);
         return -EINVAL;
     }
     return 0;
