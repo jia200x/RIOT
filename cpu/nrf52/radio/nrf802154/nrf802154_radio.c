01cbf87b9491c1079de5b37967f474c82efb5e3b
Jose Alamos <jose@alamos.cc>
2022-06-01 14:55:27 +0200

    REMOVE_ME: nrf802154: adapt radio and fixes

--- cpu/nrf52/radio/nrf802154/nrf802154_radio.c
+++ cpu/nrf52/radio/nrf802154/nrf802154_radio.c
@@ -118,11 +118,14 @@ static void _power_off(void)
 
 static bool _l2filter(uint8_t *mhr)
 {
+    uint8_t src_addr[IEEE802154_LONG_ADDRESS_LEN];
     uint8_t dst_addr[IEEE802154_LONG_ADDRESS_LEN];
     le_uint16_t dst_pan;
+    le_uint16_t src_pan;
     uint8_t pan_bcast[] = IEEE802154_PANID_BCAST;
 
     int addr_len = ieee802154_get_dst(mhr, dst_addr, &dst_pan);
+    ieee802154_get_src(mhr, src_addr, &src_pan);
 
     if ((mhr[0] & IEEE802154_FCF_TYPE_MASK) == IEEE802154_FCF_TYPE_BEACON) {
         if ((memcmp(&nrf802154_pan_id, pan_bcast, 2) == 0)) {
